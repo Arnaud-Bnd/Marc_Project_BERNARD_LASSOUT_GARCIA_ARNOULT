d17244cd533e7feb276790b4839e9d6ab879152f
ArnoultL <150684124+ArnoultL@users.noreply.github.com>
2024-10-23 10:38:32 +0200

    trees debut

---
+++ tree_functions.c
@@ -0,0 +1,9 @@
+//
+// Created by Laetitia Arnoult on 23/10/2024.
+//
+
+#include "tree_functions.h"
+
+t_node *createNode(int val, int nbSons){
+
+}
---
+++ tree_functions.h
@@ -0,0 +1,22 @@
+//
+// Created by Laetitia Arnoult on 23/10/2024.
+//
+
+#ifndef MARC_PROJECT_BERNARD_LASSOUT_GARCIA_ARNOULT_TREE_FUNCTIONS_H
+#define MARC_PROJECT_BERNARD_LASSOUT_GARCIA_ARNOULT_TREE_FUNCTIONS_H
+
+typedef struct s_node{
+    int val;//valeur stockée dans le noeud
+    struct s_node **sons;
+    int nbSons; // nombre de fils fils
+}t_node;
+
+typedef struct s_n_node{
+    int value;
+    t_node sons;
+}t_n_node;
+
+t_node *createNode(int val, int nbSons);
+
+
+#endif //MARC_PROJECT_BERNARD_LASSOUT_GARCIA_ARNOULT_TREE_FUNCTIONS_H
